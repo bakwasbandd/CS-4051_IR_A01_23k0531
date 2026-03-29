490329fb00ae5d1d7b421864c619296648eb4e4d
bakwasbandd <muntahaadnan2005@gmail.com>
2026-03-29 20:47:04 +0500

    commented

--- main.py
+++ main.py
@@ -6,42 +6,53 @@ SPEECHES_FOLDER = "Speeches"
 STOPWORDS_FILE = "Stopword-List.txt"
 
 
+# build() is our index creator
 def build():
     stopwords = load_stopwords(STOPWORDS_FILE)
+
+    # build and save indexes
     inv, pos, doc_mapping = build_indexes(SPEECHES_FOLDER, stopwords)
-    save_indexes(inv, pos, doc_mapping)
+    save_indexes(inv, pos, doc_mapping) # saving so we dont have to redo it everytime
+
+    # to make sure everything is working! printing stats
     print(f"Done! Indexed {len(doc_mapping)} documents, {len(inv)} unique terms.")
 
 
 def search():
-    inv, pos, doc_mapping = load_indexes()
+    inv, pos, doc_mapping = load_indexes() # load the saved indexes
 
+    # making a set of all doc ids (useful for NOT queries)
     all_docs = set(doc_mapping.keys())
 
     print(f"Loaded {len(doc_mapping)} docs, {len(inv)} terms.")
 
     while True:
-        q = input("\nEnter query (or 'exit'): ").strip()
+        q = input("\nEnter query (or 'exit' to stop searching): ").strip()
 
         if q.lower() == "exit":
             break
 
+        # Determine if it's a proximity query (contains "/") or a boolean query
+        # Proximity queries are of the form: "term1 term2 /k" (e.g., "freedom speech /3")
+        # use positional index for proximity queries, inverted index for boolean queries
         if "/" in q:
             result_ids = process_proximity(q, pos)
         else:
             result_ids = process_boolean(q, inv, all_docs)
 
+
+        # printing the docs returned by the query processor
         if result_ids:
             print(f"\n{len(result_ids)} document(s) found:")
             for doc_id in sorted(result_ids):
                 print(f"  [{doc_id}] {doc_mapping[doc_id]}")
         else:
-            print("No documents found.")
+            print("No documents found.") # nothing returned!
 
 
 if __name__ == "__main__":
     while True:
-        choice = input("\n1. Build Index\n2. Search\n3. Exit\nChoose: ")
+        choice = input("\n1. Build the Indexes [Inverted and Positional]\n2. Search\n3. Exit\n\nChoose: ")
 
         if choice == "1":
             build()
@@ -50,4 +61,4 @@ if __name__ == "__main__":
         elif choice == "3":
             break
         else:
-            print("Invalid choice.")
+            print("Invalid choice, Please enter 1, 2, or 3 :(")
